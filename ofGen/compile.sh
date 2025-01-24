7db1824686e39b69b223dbb9beede975a79c6f49
Dimitre <dimitre@users.noreply.github.com>
2025-01-24 14:16:27 -0300

    script detects if it is local, so it runs ok on CI

--- ofGen/compile.sh
+++ ofGen/compile.sh
@@ -1,22 +1,18 @@
 #!/bin/zsh
 cd "$(dirname "$0")"
 
-# time g++ -Wfatal-errors -std=c++20 `pkg-config --libs --cflags fmt yaml-cpp` src/main.cpp -o ofGen -O3
-mkdir -p obj
+pwd
 
-#PROJECT_CXX = /opt/homebrew/opt/llvm/bin/clang++
-#PROJECT_CC = /opt/homebrew/opt/llvm/bin/clang
-#PROJECT_LD = /opt/homebrew/opt/llvm/bin/clang++ -fuse-ld=lld
+if [[ ${PWD} == "/Volumes/tool/ofw/ofGen" ]] then
 time ccache /opt/homebrew/opt/llvm/bin/clang++ -c src/*.cpp -Wfatal-errors -std=c++17
-
-# time ccache c++ -c -Wfatal-errors -std=c++20 src/utils.cpp -o obj/utils.o -O3
-# time ccache c++ -c -Wfatal-errors -std=c++20 src/main.cpp -o obj/main.o -O3
-
-# time ccache c++ -c -Wfatal-errors -std=c++20 src/addons.cpp
-# time ccache c++ -c -Wfatal-errors -std=c++20 src/utils.cpp
-# time ccache c++ -c -Wfatal-errors -std=c++20 src/main.cpp
-
 time ccache /opt/homebrew/opt/llvm/bin/clang++ -fuse-ld=lld *.o -o ofGen && time ./ofGen
+# time include-what-you-use -c src/*.cpp -Wfatal-errors -std=c++17
+# time include-what-you-use -fuse-ld=lld *.o -o ofGen && time ./ofGen
+else
+time ccache c++ -c src/*.cpp -Wfatal-errors -std=c++17
+time ccache c++ *.o -o ofGen && time ./ofGen
+fi
+
 # include-what-you-use g++ -Wfatal-errors -std=c++20 src/main.cpp src/utils.cpp -o ofGen -O3
 # ./ofGen
 #path+=$PWD
