bae2f8eb3c1c85d4f286b99e8b4dafd3bd1ac7a7
Dimitre <dimitre@users.noreply.github.com>
2025-01-28 12:06:48 -0300

    fix ofGen script?

--- ofGen/compile.sh
+++ ofGen/compile.sh
@@ -6,11 +6,13 @@ pwd
  # # time include-what-you-use -c src/*.cpp -Wfatal-errors -std=c++17
  # time include-what-you-use -fuse-ld=lld *.o -o ofGen && time ./ofGen
 CXX=c++
+LINKEROPTIONS=
 if [[ ${PWD} == "/Volumes/tool/ofw/ofGen" ]] then
 # CXX=/opt/homebrew/opt/llvm/bin/clang++
+LINKEROPTIONS=-fuse-ld=lld
 fi
 time ccache $CXX -c src/*.cpp src/uuidxx/src/*.cpp -Isrc/uuidxx/src -Wfatal-errors -std=c++20
-time ccache $CXX -fuse-ld=lld *.o -Isrc/uuidxx/src -o ofGen && time ./ofGen
+time ccache $CXX $LINKEROPTIONS *.o -Isrc/uuidxx/src -o ofGen && time ./ofGen
 
 # include-what-you-use g++ -Wfatal-errors -std=c++20 src/main.cpp src/utils.cpp -o ofGen -O3
 # ./ofGen
