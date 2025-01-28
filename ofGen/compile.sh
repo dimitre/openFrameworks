200491db767612e64730d2c5ad99167482c0c16a
Dimitre <dimitre@users.noreply.github.com>
2025-01-28 11:53:41 -0300

    fix script for ci

--- ofGen/compile.sh
+++ ofGen/compile.sh
@@ -9,8 +9,8 @@ time ccache /opt/homebrew/opt/llvm/bin/clang++ -fuse-ld=lld *.o -Isrc/uuidxx/src
 # time include-what-you-use -c src/*.cpp -Wfatal-errors -std=c++17
 # time include-what-you-use -fuse-ld=lld *.o -o ofGen && time ./ofGen
 else
-time ccache c++ -c src/*.cpp -Wfatal-errors -std=c++20
-time ccache c++ *.o -o ofGen && time ./ofGen
+time ccache c++ -c src/*.cpp -Isrc/uuidxx/src -Wfatal-errors -std=c++20
+time ccache c++ *.o -Isrc/uuidxx/src -o ofGen && time ./ofGen
 fi
 
 # include-what-you-use g++ -Wfatal-errors -std=c++20 src/main.cpp src/utils.cpp -o ofGen -O3
