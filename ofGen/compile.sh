ef45c1c96e49b2f9bb1039e0e531f661631db7de
Dimitre <dimitre@users.noreply.github.com>
2025-01-28 12:04:02 -0300

    fix ofGen script?

--- ofGen/compile.sh
+++ ofGen/compile.sh
@@ -3,15 +3,14 @@ cd "$(dirname "$0")"
 
 pwd
  # -lstdc++fs
+ # # time include-what-you-use -c src/*.cpp -Wfatal-errors -std=c++17
+ # time include-what-you-use -fuse-ld=lld *.o -o ofGen && time ./ofGen
+CXX=c++
 if [[ ${PWD} == "/Volumes/tool/ofw/ofGen" ]] then
-time ccache /opt/homebrew/opt/llvm/bin/clang++ -c src/*.cpp src/uuidxx/src/*.cpp -Isrc/uuidxx/src -Wfatal-errors -std=c++20
-time ccache /opt/homebrew/opt/llvm/bin/clang++ -fuse-ld=lld *.o -Isrc/uuidxx/src -o ofGen && time ./ofGen
-# time include-what-you-use -c src/*.cpp -Wfatal-errors -std=c++17
-# time include-what-you-use -fuse-ld=lld *.o -o ofGen && time ./ofGen
-else
-time ccache c++ -c src/*.cpp -Isrc/uuidxx/src -Wfatal-errors -std=c++20
-time ccache c++ *.o -Isrc/uuidxx/src -o ofGen && time ./ofGen
+# CXX=/opt/homebrew/opt/llvm/bin/clang++
 fi
+time ccache $CXX -c src/*.cpp src/uuidxx/src/*.cpp -Isrc/uuidxx/src -Wfatal-errors -std=c++20
+time ccache $CXX -fuse-ld=lld *.o -Isrc/uuidxx/src -o ofGen && time ./ofGen
 
 # include-what-you-use g++ -Wfatal-errors -std=c++20 src/main.cpp src/utils.cpp -o ofGen -O3
 # ./ofGen
