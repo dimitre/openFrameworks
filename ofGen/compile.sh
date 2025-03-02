15af06c819ec9ca8b800ec46fd18dedd671093d8
Dimitre <dimitre@users.noreply.github.com>
2025-03-02 20:00:22 -0300

    pkgconf

--- ofGen/compile.sh
+++ ofGen/compile.sh
@@ -31,18 +31,25 @@ fi
 
 section "Will compile now"
 
-make -j
+if [[ -z $GITHUB_REPOSITORY ]]; then
+    section "Using Make"
+    make -j
+else
+    echo "GITHUB_REPOSITORY, compiling oldstyle"
 
-# CXX=c++
-# LINKEROPTIONS=
-# if [[ ${PWD} == "/Volumes/tool/ofw/ofGen" ]] then
-# CXX=/opt/homebrew/opt/llvm/bin/clang++
-# LINKEROPTIONS=-fuse-ld=lld
+    CXX=c++
+    LINKEROPTIONS=
+    if [[ ${PWD} == "/Volumes/tool/ofw/ofGen" ]] then
+    CXX=/opt/homebrew/opt/llvm/bin/clang++
+    LINKEROPTIONS=-fuse-ld=lld
+    fi
+
+    time $CXX -c src/*.cpp src/uuidxx/src/*.cpp `pkg-config --cflags yaml-cpp` -Isrc/uuidxx/src -I../libs/macos/include/ -Wfatal-errors -std=c++20 && \
+    time $CXX $LINKEROPTIONS *.o -Isrc/uuidxx/src `pkg-config --libs yaml-cpp` -o ofGen && \
+fi
+
+# if [[ -n $GITHUB_REPOSITORY ]]; then
 # fi
-
-# time $CXX -c src/*.cpp src/uuidxx/src/*.cpp `pkg-config --cflags yaml-cpp` -Isrc/uuidxx/src -I../libs/macos/include/ -Wfatal-errors -std=c++20 && \
-# time $CXX $LINKEROPTIONS *.o -Isrc/uuidxx/src `pkg-config --libs yaml-cpp` -o ofGen && \
-
 # LEFTOVERS
 #
 #
