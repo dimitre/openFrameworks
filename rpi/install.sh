984240cbbaa3b5dfc85e98681ecf92aeaf5c6d6a
Dimitre <dimitre@users.noreply.github.com>
2025-03-07 13:51:44 -0300

    up scripts

--- rpi/install.sh
+++ rpi/install.sh
@@ -7,4 +7,9 @@ sudo apt install -y multistrap gcc-${TOOLCHAIN} g++-${TOOLCHAIN}
 
 mkdir -p raspbian/etc/apt/apt.conf.d/
 echo 'Acquire::AllowInsecureRepositories "true";' | sudo tee raspbian/etc/apt/apt.conf.d/90insecure
+
+# if [[ ${TOOLCHAIN} = "arm-linux-gnueabihf" ]]; then
+# 	git clone https://github.com/raspberrypi/userland --depth 1 raspbian/userland
+# fi
+
 multistrap -a arm64 -d raspbian -f multistrap.conf
