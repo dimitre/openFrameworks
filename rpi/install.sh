73319fdc2339641982d2b60d488f819cd77b5311
Dimitre <dimitre@users.noreply.github.com>
2025-03-07 14:16:32 -0300

    cross libs

--- rpi/install.sh
+++ rpi/install.sh
@@ -3,7 +3,7 @@ cd "$(dirname "$0")"
 
 # arm-linux-gnueabihf
 TOOLCHAIN=aarch64-linux-gnu
-sudo apt install -y multistrap gcc-${TOOLCHAIN} g++-${TOOLCHAIN}
+sudo apt install -y make multistrap gcc-${TOOLCHAIN} g++-${TOOLCHAIN}
 
 mkdir -p raspbian/etc/apt/apt.conf.d/
 echo 'Acquire::AllowInsecureRepositories "true";' | sudo tee raspbian/etc/apt/apt.conf.d/90insecure
---
+++ rpi/libs.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+cd "$(dirname "$0")"
+../scripts/linux/debian/install_dependencies.sh
+# ../scripts/linux/debian/install_codecs.sh
