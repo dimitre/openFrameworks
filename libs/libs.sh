f00033107f1b5c66bd03ed08bb3e6c627a6f7cd0
Dimitre <dimitre@users.noreply.github.com>
2025-04-03 21:38:49 -0300

    libs for linux?

--- libs/libs.sh
+++ libs/libs.sh
@@ -1,6 +1,26 @@
 #!/bin/bash
 cd "$(dirname "$0")"
 
+
+COLOR='\033[0;32m'
+COLOR2='\033[0;34m'
+NC='\033[0m' # No Color
+
+section() {
+    printf "⚡️${COLOR} ${@} ${NC}\n\r"
+}
+sectionOK() {
+    printf "💾${COLOR} ${@} ${NC}\n\r"
+}
+executa2() {
+    printf "✅${COLOR2} ${@} ${NC}\n\r"
+}
+executa() { #echoes and execute. dry run is "executa2"
+    printf "✅${COLOR2} ${@} ${NC}\n\r"
+    $@
+}
+
+
 VERSION=v0.12.1
 OF_FOLDER=..
 # PLATFORM=macos
@@ -34,6 +54,8 @@ elif [[ "$(uname -s)" == "Linux" ]]; then
 	CORELIBS=( kissfft )
 	ADDONLIBS=(  )
 	ALLLIBS="${CORELIBS[@]} ${ADDONLIBS[@]}"
+	section Linux
+	uname -s
 
 	if [[ "$(uname -s)" == "x86_64" ]]; then
 		PLATFORM=linux64
@@ -62,24 +84,6 @@ LIBS_FOLDER=./${PLATFORM}
 
 DOWNLOAD="./_download_${VERSION}"
 # echo ${DOWNLOAD}
-
-COLOR='\033[0;32m'
-COLOR2='\033[0;34m'
-NC='\033[0m' # No Color
-
-section() {
-    printf "⚡️${COLOR} ${@} ${NC}\n\r"
-}
-sectionOK() {
-    printf "💾${COLOR} ${@} ${NC}\n\r"
-}
-executa2() {
-    printf "✅${COLOR2} ${@} ${NC}\n\r"
-}
-executa() { #echoes and execute. dry run is "executa2"
-    printf "✅${COLOR2} ${@} ${NC}\n\r"
-    $@
-}
 
 checkLib() {
 	for lib in "$@"
