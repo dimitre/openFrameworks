96df6d67c8ea9813de667d2ca21db369d314bb33
Dimitre <dimitre@users.noreply.github.com>
2025-08-14 11:35:42 -0300

    libs msys2 fix

--- libs/libs.sh
+++ libs/libs.sh
@@ -45,7 +45,8 @@ done
 # echo ${PLATFORM}
 # exit
 
-if [[ "$OSTYPE" == "msys"* ]]; then
+# if [[ "$OSTYPE" == "msys"* ]]; then
+if [[ "$OSTYPE" == "cygwin"* ]]; then
     # PLATFORM=windows
     PLATFORM=vs
     # CORELIBS=( brotli cairo FreeImage freetype glew glfw glm json libpng pugixml rtAudio tess2 uriparser utfcpp zlib openssl curl pixman )
