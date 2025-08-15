9d642914a374e8eab8258b922553d0274c54bed8
Dimitre <dimitre@users.noreply.github.com>
2025-08-15 01:01:30 -0300

    libs.sh msys2

--- libs/libs.sh
+++ libs/libs.sh
@@ -177,7 +177,7 @@ getlink() {
 
     if [[ "$OSTYPE" == "cygwin"* ]]; then
         executa "cd ${DOWNLOAD}"
-        executa "curl -o ${PARAMS}"
+        executa "curl -OL ${PARAMS}"
         executa "cd .."
     else
         executa "wget2 --clobber=off ${PARAMS} -P ${DOWNLOAD}"
