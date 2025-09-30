569c106ed923ece7f01c2c141d45e157bab3cf86
Dimitre <dimitre@users.noreply.github.com>
2025-09-30 13:02:56 -0300

    update chalet template for linux

--- examples/3d/3DPrimitivesExample/src/main.cpp
+++ examples/3d/3DPrimitivesExample/src/main.cpp
@@ -3,6 +3,8 @@
 //--------------------------------------------------------------
 int main(){
 
+
+
 	//Use ofGLFWWindowSettings for more options like multi-monitor fullscreen
 	ofGLWindowSettings settings;
 	settings.setSize(1024, 768);
