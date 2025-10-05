a9cdbd1089df1b6190facfce5d0476c39026acd6
Dimitre <dimitre@users.noreply.github.com>
2025-10-05 18:05:57 -0300

    makefiles for linux

--- libs/openFrameworksCompiled/project/linux64/config.linux64.default.mk
+++ libs/openFrameworksCompiled/project/linux64/config.linux64.default.mk
@@ -27,4 +27,9 @@
 
 include $(OF_SHARED_MAKEFILES_PATH)/config.linux.common.mk
 
-PLATFORM_LDFLAGS += -fuse-ld=gold
+#PLATFORM_LDFLAGS += -fuse-ld=gold
+ifneq (, $(shell command -v mold))
+	PLATFORM_LDFLAGS += -fuse-ld=mold
+else ifneq (, $(shell command -v gold))
+	PLATFORM_LDFLAGS += -fuse-ld=gold
+endif
