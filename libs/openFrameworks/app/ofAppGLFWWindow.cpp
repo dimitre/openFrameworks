7aead448e30427e1ab6e295cbf2029f502deefe0
Dimitre <dimitre@users.noreply.github.com>
2025-10-05 22:25:05 -0300

    glfw wayland fix?

--- libs/openFrameworks/app/ofAppGLFWWindow.cpp
+++ libs/openFrameworks/app/ofAppGLFWWindow.cpp
@@ -112,7 +112,12 @@ void ofAppGLFWWindow::setup(const ofWindowSettings & _settings) {
 
 
 	glfwDefaultWindowHints();
+	
+#ifdef TARGET_LINUX
+	glfwInitHint(GLFW_PLATFORM, GLFW_PLATFORM_X11);
+#endif
 
+	
 	glfwWindowHint(GLFW_COCOA_RETINA_FRAMEBUFFER, settings.highResolutionCapable);
 
 	glfwWindowHint(GLFW_RED_BITS, settings.redBits);
