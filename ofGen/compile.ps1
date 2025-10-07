7111dda1c3aac418b9758a53503a7c66a1573544
Dimitre <dimitre@users.noreply.github.com>
2025-10-07 14:46:01 -0300

    update .ps1

--- ofGen/compile.ps1
+++ ofGen/compile.ps1
@@ -1,2 +1,15 @@
 chalet bundle
-(Resolve-Path .\dist).Path | Out-File -FilePath $env:GITHUB_PATH -Encoding utf8 -Append
+$dist = (Resolve-Path .\dist).Path
+
+# 1. persist to USER scope
+$userPath = [Environment]::GetEnvironmentVariable('Path','User')
+if ($userPath -split [IO.Path]::PathSeparator -notcontains $dist) {
+    [Environment]::SetEnvironmentVariable(
+        'Path',
+        $userPath + [IO.Path]::PathSeparator + $dist,
+        'User'
+    )
+}
+
+# 2. refresh this process
+$env:Path = [Environment]::GetEnvironmentVariable('Path','User')
