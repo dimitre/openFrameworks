bc866f1dd82a52cf3cde7c22412890b87ddb058e
Dimitre <dimitre@users.noreply.github.com>
2025-10-27 21:32:38 -0300

    update ofStart

--- ofStart.sh
+++ ofStart.sh
@@ -2,58 +2,10 @@
 cd "$(dirname "$0")"
 set -e
 
+./libs/libs.sh
+cd ofGen
+./compile.sh
+cd ../examples/demos/flechilhas
+ofgen buildrun
 
-if [[ "$(uname -s)" == "Darwin" ]]; then
-    if ! command -v brew 2>&1 >/dev/null
-    then
-        echo "Brew not installed, install via instructions in the browser and then run the script again"
-        open https://brew.sh
-        exit
-    fi
-fi
-COLOR='\033[0;32m'
-COLOR2='\033[0;34m'
-NC='\033[0m' # No Color
-
-section() {
-    printf "💻 ${COLOR}${@}${NC}\n\r"
-}
-
-# echo "parameter"
-section "OFWorks (OpenFrameworks Fork)"
-echo $@
-
-if [ -d "ofworks" ]; then
-    echo "Folder ${PWD}/ofworks exists, exiting"
-    exit
-fi
-
-echo "Installing to ${PWD}/ofworks"
-read -p "Proceed? (y/n) " -n 1 -r
-
-
-echo    # (optional) move to a new line
-if [[ $REPLY =~ ^[Yy]$ ]]
-then
-	echo "YES"
-	time git clone --branch ofworks https://github.com/dimitre/openframeworks --depth 1 ofworks
-	cd ofworks
-
-
-	./libs/libs.sh
-	cd ofGen
-	./compile.sh
-	cd ../examples/demos/flechilhas
-	ofgen buildrun
-
-	echo "All good!"
-
-	cd ..
-else
-	echo "ok, exit without installing."
-    # do dangerous stuff
-fi
-
-
-## BREW ALIKE
-# /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/dimitre/openFrameworks/refs/heads/tuningfork/install_of.sh)" pwd
+cd ../../..
