c10f3677469c19bf75ddea644a6255c57b5ce33d
Dimitre <dimitre@users.noreply.github.com>
2025-10-27 22:14:30 -0300

    update libs.sh

--- libs/libs.sh
+++ libs/libs.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 cd "$(dirname "$0")"
+set -eu
 
 VERSION=v0.12.3
 OF_FOLDER=..
-# PLATFORM=macos
-#rpi-aarch64.zip
 
 wipeDownloads=true
 wipeAddonLibs=true
@@ -13,316 +12,250 @@ wipeLibs=true
 COLOR='\033[0;32m'
 COLOR2='\033[0;34m'
 COLOR3='\033[0;95m'
-
 NC='\033[0m' # No Color
 
 section() {
-	printf "⚡️ ${COLOR}$@${NC}\n"
+    printf "⚡️ ${COLOR}%s${NC}\n" "$*"
 }
+
 sectionOK() {
-	printf "💾 ${COLOR}$@${NC}\n"
+    printf "💾 ${COLOR}%s${NC}\n" "$*"
 }
+
 executa2() {
-	printf "✅ ${COLOR2}$@${NC}\n"
-}
-executa() { #echoes and execute. dry run is "executa2"
-	printf "✅ ${COLOR2}$@${NC}\n"
-	$@
+    printf "✅ ${COLOR2}%s${NC}\n" "$*"
 }
 
+executa() {
+    printf "✅ ${COLOR2}%s${NC}\n" "$*"
+    "$@"
+}
 
-
-for i in "$@"; do
-	case $i in
-	-p=*|--platform=*)
-		PLATFORM="${i#*=}"
-		shift # past argument=value
-		;;
-	-l=*|--searchpath=*)
-		SEARCHPATH="${i#*=}"
-		shift # past argument=value
-		;;
-	esac
+# Parse command line arguments
+PLATFORM="${PLATFORM:-}"
+while [[ $# -gt 0 ]]; do
+    case $1 in
+    -p=*|--platform=*)
+        PLATFORM="${1#*=}"
+        shift
+        ;;
+    -p|--platform)
+        PLATFORM="$2"
+        shift 2
+        ;;
+    *)
+        echo "Unknown option: $1"
+        exit 1
+        ;;
+    esac
 done
 
-# LIBS_FOLDER=${OF_FOLDER}/libs/${PLATFORM}
-# echo ${PLATFORM}
-# exit
+# ============================================
+# Platform Detection
+# ============================================
+if [[ -n "$PLATFORM" ]]; then
+    section "Using manually specified platform: $PLATFORM"
+else
+    # Auto-detect platform
+    if [[ "$OSTYPE" == "msys"* ]]; then
+        PLATFORM=vs
+    elif [[ "$OSTYPE" == "cygwin"* ]]; then
+        PLATFORM=msys2
+    elif [[ "$OSTYPE" == "darwin"* ]]; then
+        PLATFORM=macos
+    elif [[ "$OSTYPE" == "linux-gnu"* ]]; then
+        # Detect Linux variant
+        ARCH="$(uname -m)"
 
-# if [[ "$OSTYPE" == "msys"* ]]; then
-# This is MSYS / MING Windows
-
-# echo "ostype = ${OSTYPE}"
-
-# VS - Visual Studio
-if [[ "$OSTYPE" == "msys"* ]]; then
-    PLATFORM=vs
-    #libjpeg libtiff FreeImage pixman
-   	CORELIBS=( mango videoInput yaml-cpp brotli freetype brotli glew glfw glm json libpng pugixml rtAudio tess2 uriparser utfcpp zlib-ng )
-	# FIXME: TODO: add svgtiny to ofLibs and here
-	# TODO: curl, openssl
-	ADDONLIBS=( assimp cairo libusb opencv )
-	ALLLIBS="${CORELIBS[@]} ${ADDONLIBS[@]}"
-
-	LIBADDONS=(
-		# "assimp:ofxAssimpModelLoader"
-		"assimp:ofxAssimp"
-		# "cairo:ofxCairo"
-		"libusb:ofxKinect"
-		"opencv:ofxOpenCv"
-	)
-
-# Windows MSYS2
-elif [[ "$OSTYPE" == "cygwin"* ]]; then
-	PLATFORM=msys2
-	CORELIBS=( tess2 kissfft videoInput )
-	# CORELIBS=( brotli cairo FreeImage freetype glew glfw glm json libpng pugixml rtAudio tess2 uriparser utfcpp zlib openssl curl pixman )
-	ADDONLIBS=( )
-	ALLLIBS="${CORELIBS[@]} ${ADDONLIBS[@]}"
-	# LIBADDONS=(
-	# 	# "assimp:ofxAssimpModelLoader"
-	# 	"assimp:ofxAssimp"
-	#	 "libusb:ofxKinect"
-	#	 "libxml2:ofxSvg"
-	#	 "opencv:ofxOpenCv"
-	#	 # "svgtiny:ofxSvg"
-	# )
-	# gcc
-	PACMANLIBS="toolchain openssl python assimp cairo curl freeglut FreeImage glew glfw glm libsndfile libusb libxml2 mpg123 nlohmann-json openal opencv pugixml rtaudio uriparser utf8cpp"
-
-	PACMANPARAMS="pacman -Syyuw --noconfirm"
-	for LIBNAME in ${PACMANLIBS[@]}
-	do
-		PACMANPARAMS+=" mingw-w64-x86_64-${LIBNAME}"
-	done
-
-	# FIXME VOLTAR
-	# executa "${PACMANPARAMS}"
-
-# macOS
-elif [[ "$(uname -s)" == "Darwin" ]]; then
-	PLATFORM=macos
-	# lzma libtiff libjpeg  pixman FreeImage
-	CORELIBS=( libpng fmt mango yaml-cpp brotli freetype glew glfw glm json pugixml rtAudio tess2 uriparser utfcpp zlib-ng  )
-	# FIXME: TODO: add svgtiny to ofLibs and here
-	# ADDONLIBS=( assimp cairo libusb opencv openssl curl )
-	ADDONLIBS=( assimp opencv libusb ) # cairo openssl curl
-	ALLLIBS="${CORELIBS[@]} ${ADDONLIBS[@]}"
-
-	LIBADDONS=(
-		# "assimp:ofxAssimpModelLoader"
-		"assimp:ofxAssimp"
-		# "cairo:ofxCairo"
-		"libusb:ofxKinect"
-		# "libxml2:ofxSvg"
-		"opencv:ofxOpenCv"
-		# "openssl:ofxURL"
-		# "curl:ofxURL"
-		# "svgtiny:ofxSvg"
-	)
-
-
-elif [[ "$(uname -s)" == "Linux" ]]; then # UBUNTU
-	# libxml2-dev
-	# sudo apt-get -y install libcairo2-dev make libgtk2.0-dev nlohmann-json3-dev libssl3 libcurl4 brotli libcurl4-openssl-dev libjack-jackd2-0 libjack-jackd2-dev freeglut3-dev libasound2-dev libxmu-dev libxxf86vm-dev libgl1-mesa-dev libraw1394-dev libudev-dev libdrm-dev libglew-dev libopenal-dev libsndfile1-dev libfreeimage-dev libcairo2-dev libfreetype6-dev libssl-dev libpulse-dev libusb-1.0-0-dev libopencv-dev libassimp-dev librtaudio-dev liburiparser-dev libpugixml-dev libgtk2.0-0 libxcursor-dev libxi-dev libxinerama-dev libglfw3-dev libgstreamer1.0-dev libgstreamer-plugins-base1.0-dev gstreamer1.0-pulseaudio gstreamer1.0-x gstreamer1.0-plugins-bad gstreamer1.0-alsa gstreamer1.0-plugins-base gstreamer1.0-plugins-good gstreamer1.0-libav
-	# CORELIBS=( kissfft glm tess2 utfcpp )
-	# ADDONLIBS=(	)
-
-	if [[ -z "$CI" ]]; then
-	    #libgtk2.0-dev libgtk2.0-0
-					# libraw1394-dev # firewire support, legacy
-					# libpulse-dev gstreamer1.0-pulseaudio libjack-jackd2-0 libjack-jackd2-dev
-					#
-					# libdrm-dev
-					#
-					# libraw1394-dev libdrm-dev libopenal-dev libpulse-dev gstreamer1.0-pulseaudio libjack-jackd2-0 libjack-jackd2-dev
-		sudo apt-get -y install make \
-		libssl3 libcairo2-dev libssl-dev libcurl4 libcurl4-openssl-dev \
-		libasound2-dev libsndfile1-dev \
-		libopenal-dev \
-		freeglut3-dev libxmu-dev libxxf86vm-dev libgl1-mesa-dev libudev-dev libxcursor-dev libxi-dev libxinerama-dev \
-		libgstreamer1.0-dev libgstreamer-plugins-base1.0-dev \
-		gstreamer1.0-x gstreamer1.0-plugins-bad gstreamer1.0-alsa gstreamer1.0-plugins-base gstreamer1.0-plugins-good gstreamer1.0-libav
-	fi
-	# Fixme. Brotli will have to be re-added when issues are fixed # brotli
-	# libtiff libjpeg FreeImage pixman
-	CORELIBS=( mango yaml-cpp kissfft freetype glew glfw glm json libpng pugixml rtAudio tess2 uriparser utfcpp zlib-ng  )
-
-	ADDONLIBS=(	assimp libusb opencv )
-	LIBADDONS=(
-		# "assimp:ofxAssimpModelLoader"
-		"assimp:ofxAssimp"
-		# "cairo:ofxCairo"
-		"libusb:ofxKinect"
-		# "libxml2:ofxSvg"
-		"opencv:ofxOpenCv"
-		# "openssl:ofxURL"
-		# "curl:ofxURL"
-		# "svgtiny:ofxSvg"
-	)
-
-
-	ALLLIBS="${CORELIBS[@]} ${ADDONLIBS[@]}"
-	section Linux
-	uname -m
-
-	if [[ "$(uname -m)" == "x86_64" ]]; then
-		PLATFORM=linux64
-	elif [ -f /etc/rpi-issue ]; then
-		PLATFORM=rpi-aarch64
-	fi
+        if [[ "$ARCH" == "x86_64" ]]; then
+            PLATFORM=linux64
+        elif [[ "$ARCH" == "aarch64" ]] && [[ -f /etc/rpi-issue ]]; then
+            PLATFORM=rpi-aarch64
+        elif [[ "$ARCH" == "armv6l" ]] || [[ "$ARCH" == "armv7l" ]]; then
+            # Check if it's actually a Raspberry Pi
+            if [[ -f /proc/device-tree/model ]] && grep -q "Raspberry Pi" /proc/device-tree/model; then
+                PLATFORM=rpi-armv6l
+            else
+                echo "Unsupported ARM architecture: $ARCH (not a Raspberry Pi?)"
+                exit 1
+            fi
+        else
+            echo "Unsupported Linux architecture: $ARCH"
+            exit 1
+        fi
+    else
+        echo "Unsupported OS: $OSTYPE"
+        exit 1
+    fi
+    section "Detected platform: $PLATFORM"
 fi
 
+# ============================================
+# Platform-Specific Configuration
+# ============================================
+case "$PLATFORM" in
+    vs)
+        CORELIBS=( mango videoInput yaml-cpp brotli freetype glew glfw glm json libpng pugixml rtAudio tess2 uriparser utfcpp zlib-ng )
+        ADDONLIBS=( assimp cairo libusb opencv )
+        LIBADDONS=(
+            "assimp:ofxAssimp"
+            "libusb:ofxKinect"
+            "opencv:ofxOpenCv"
+        )
+        ;;
 
-# FIXME: Remove, convert to function
-if [[ $1 == 'vs' ]]; then
-    PLATFORM=$1
-   	CORELIBS=( yaml-cpp brotli FreeImage freetype glew glfw glm json libpng pixman pugixml rtAudio tess2 uriparser utfcpp zlib )
-	# FIXME: TODO: add svgtiny to ofLibs and here
-	# TODO: curl, openssl
-	ADDONLIBS=( assimp cairo libusb opencv )
-	ALLLIBS="${CORELIBS[@]} ${ADDONLIBS[@]}"
+    msys2)
+        CORELIBS=( tess2 kissfft videoInput )
+        ADDONLIBS=()
+        LIBADDONS=()
 
-	LIBADDONS=(
-		# "assimp:ofxAssimpModelLoader"
-		"assimp:ofxAssimp"
-		"cairo:ofxCairo"
-		"libusb:ofxKinect"
-		"opencv:ofxOpenCv"
-	)
-fi
-# PLATFORM="${PLATFORM:-macos}"
+        # Install MSYS2 packages
+        PACMANLIBS="toolchain openssl python assimp cairo curl freeglut FreeImage glew glfw glm libsndfile libusb libxml2 mpg123 nlohmann-json openal opencv pugixml rtaudio uriparser utf8cpp"
+        PACMANPARAMS="pacman -Syyuw --noconfirm"
+        for LIBNAME in ${PACMANLIBS}; do
+            PACMANPARAMS+=" mingw-w64-x86_64-${LIBNAME}"
+        done
+        # executa ${PACMANPARAMS}
+        ;;
 
+    macos)
+        CORELIBS=( libpng fmt mango yaml-cpp brotli freetype glew glfw glm json pugixml rtAudio tess2 uriparser utfcpp zlib-ng )
+        ADDONLIBS=( assimp opencv libusb )
+        LIBADDONS=(
+            "assimp:ofxAssimp"
+            "libusb:ofxKinect"
+            "opencv:ofxOpenCv"
+        )
+        ;;
 
-# unameOut="$(uname -s)"
-# case "${unameOut}" in
-#	 Linux*)	 machine=Linux;;
-#	 Darwin*)	machine=macos;;
-#	 CYGWIN*)	machine=Cygwin;;
-#	 MINGW*)	 machine=MinGw;;
-#	 MSYS_NT*)	 machine=MSys;;
-#	 *)			machine="UNKNOWN:${unameOut}"
-# esac
-# echo ${machine}
-# exit
+    linux64|rpi-aarch64|rpi-armv6l)
+        CORELIBS=( mango yaml-cpp kissfft freetype glew glfw glm json libpng pugixml rtAudio tess2 uriparser utfcpp zlib-ng )
+        ADDONLIBS=( assimp libusb opencv )
+        LIBADDONS=(
+            "assimp:ofxAssimp"
+            "libusb:ofxKinect"
+            "opencv:ofxOpenCv"
+        )
 
-LIBS_FOLDER=./${PLATFORM}
+        # Install system dependencies (skip in CI)
+        if [[ -z "${CI:-}" ]]; then
+            section "Installing system dependencies"
+            sudo apt-get -y install make \
+                libssl3 libcairo2-dev libssl-dev libcurl4 libcurl4-openssl-dev \
+                libasound2-dev libsndfile1-dev libopenal-dev \
+                freeglut3-dev libxmu-dev libxxf86vm-dev libgl1-mesa-dev libudev-dev \
+                libxcursor-dev libxi-dev libxinerama-dev \
+                libgstreamer1.0-dev libgstreamer-plugins-base1.0-dev \
+                gstreamer1.0-x gstreamer1.0-plugins-bad gstreamer1.0-alsa \
+                gstreamer1.0-plugins-base gstreamer1.0-plugins-good gstreamer1.0-libav
+        fi
+        ;;
 
-if [[ "$wipeLibs" == true && -d ${LIBS_FOLDER} ]]; then
-    executa rm -rf ${LIBS_FOLDER}
+    *)
+        echo "Unknown platform: $PLATFORM"
+        exit 1
+        ;;
+esac
+
+# Build combined library list
+ALLLIBS=("${CORELIBS[@]}" "${ADDONLIBS[@]}")
+
+section "Platform: $PLATFORM"
+section "Core libs: ${#CORELIBS[@]}"
+section "Addon libs: ${#ADDONLIBS[@]}"
+
+# ============================================
+# Setup Directories
+# ============================================
+LIBS_FOLDER="./${PLATFORM}"
+
+if [[ "$wipeLibs" == true && -d "${LIBS_FOLDER}" ]]; then
+    executa rm -rf "${LIBS_FOLDER}"
 fi
 
 DOWNLOAD="./_download_${VERSION}_${PLATFORM}"
 
-# wipe folder to re-download libs.
 if [[ "$wipeDownloads" == true && -d "${DOWNLOAD}" ]]; then
     echo "Removing Previously Downloaded Libraries"
-    rm -rf ${DOWNLOAD}
+    rm -rf "${DOWNLOAD}"
 fi
 
 echo "Creating Download Folder ${DOWNLOAD}"
-mkdir ${DOWNLOAD}
+mkdir -p "${DOWNLOAD}"
 
-
-# echo ${DOWNLOAD}
-
+# ============================================
+# Helper Functions
+# ============================================
 checkLib() {
-	for lib in "$@"
-	do
-		section Check $lib
-		if ! command -v $lib 2>&1 >/dev/null
-		then
-			if [[ ${PLATFORM} == "macos" ]]; then
-				echo "$lib not found, installing via brew"
-				executa brew install $lib
-			# FIXME: msys2 doesn't want sudo, check via other stuff.
-			else
-				echo "$lib not found, installing via apt"
-				executa sudo apt-get install -y $lib
-			fi
-		else
-			echo "$lib ok"
-		fi
-	done
+    for lib in "$@"; do
+        section "Check $lib"
+        if ! command -v "$lib" &>/dev/null; then
+            if [[ "${PLATFORM}" == "macos" ]]; then
+                echo "$lib not found, installing via brew"
+                executa brew install "$lib"
+            else
+                echo "$lib not found, installing via apt"
+                executa sudo apt-get install -y "$lib"
+            fi
+        else
+            echo "$lib ok"
+        fi
+    done
 }
 
-
-# checkWget2() {
-# section Check Wget2
-# if ! command -v wget2 2>&1 >/dev/null
-# then
-#	 echo "<wget2> not found, installing via brew"
-#	 executa brew install wget2
-# else
-# 	echo "ok"
-# fi
-# }
-
-# exit 1
-
 getlink() {
-    if [[ "$OSTYPE" == "cygwin"* || "$OSTYPE" == "msys"* ]]; then
-
-		for LIBNAME in ${ALLLIBS[@]}
-		do
-			# github uses redirect, so it is needed -L parameter in curl.
-			executa "curl -L -o ${DOWNLOAD}/ofLibs_${LIBNAME}_${PLATFORM}.zip https://github.com/dimitre/ofLibs/releases/download/${VERSION}/ofLibs_${LIBNAME}_${PLATFORM}.zip"
-		done
-	else
-
-		for LIBNAME in ${ALLLIBS[@]}
-		do
-			PARAMS+=" "https://github.com/dimitre/ofLibs/releases/download/${VERSION}/ofLibs_${LIBNAME}_${PLATFORM}.zip
-		done
-
-		executa "wget2 --clobber=off ${PARAMS} -P ${DOWNLOAD}"
-	fi
+    if command -v wget2 &>/dev/null; then
+        # wget2 is available - use it for parallel downloads
+        PARAMS=""
+        for LIBNAME in "${ALLLIBS[@]}"; do
+            PARAMS+=" https://github.com/dimitre/ofLibs/releases/download/${VERSION}/ofLibs_${LIBNAME}_${PLATFORM}.zip"
+        done
+        section "Downloading with wget2 (parallel downloads)"
+        wget2 --clobber=off ${PARAMS} -P "${DOWNLOAD}"
+    else
+        # Fallback to curl - sequential downloads
+        section "Downloading with curl (sequential)"
+        for LIBNAME in "${ALLLIBS[@]}"; do
+            executa curl -L -o "${DOWNLOAD}/ofLibs_${LIBNAME}_${PLATFORM}.zip" "https://github.com/dimitre/ofLibs/releases/download/${VERSION}/ofLibs_${LIBNAME}_${PLATFORM}.zip"
+        done
+    fi
 }
 
 unzipCore() {
-	for LIBNAME in ${CORELIBS[@]}
-	do
-		filename="${DOWNLOAD}/ofLibs_${LIBNAME}_${PLATFORM}.zip"
-		# executa unzip -o ${filename} -d ${LIBS_FOLDER}
-		# -q = quiet -qq = quieter
-		executa "unzip -qq -o ${filename} -d ${LIBS_FOLDER}"
-	done
+    for LIBNAME in "${CORELIBS[@]}"; do
+        filename="${DOWNLOAD}/ofLibs_${LIBNAME}_${PLATFORM}.zip"
+        executa unzip -qq -o "${filename}" -d "${LIBS_FOLDER}"
+    done
 
-	executa "rm -rf ${LIBS_FOLDER}/*.{txt,md,MIT}"
-	executa "rm -rf ${LIBS_FOLDER}/{LICENSE,COPYING}"
-	executa "rm -rf ${LIBS_FOLDER}/LICENSES"
-
-	# remover no futuro
-	# executa "mv ${LIBS_FOLDER}/lib/${PLATFORM}/* ${LIBS_FOLDER}/lib/"
-	# executa "rm -rf ${LIBS_FOLDER}/lib/${PLATFORM}"
+    rm -rf "${LIBS_FOLDER}"/*.{txt,md,MIT}
+    rm -rf "${LIBS_FOLDER}"/{LICENSE,COPYING,LICENSES}
 }
-
-
 
 unzipAddons() {
-	for libaddon in "${LIBADDONS[@]}" ; do
-		lib=${libaddon%%:*}
-		addon=${libaddon#*:}
-		OUTFOLDER=${OF_FOLDER}/addons/${addon}/libs/${lib}
-		wipeAddonLibs=true
-        if [ "$wipeAddonLibs" == true ]; then
-            executa "rm -rf ${OUTFOLDER}"
+    for libaddon in "${LIBADDONS[@]}"; do
+        lib=${libaddon%%:*}
+        addon=${libaddon#*:}
+        OUTFOLDER="${OF_FOLDER}/addons/${addon}/libs/${lib}"
+
+        if [[ "$wipeAddonLibs" == true ]]; then
+            executa rm -rf "${OUTFOLDER}"
         fi
-		executa "mkdir -p ${OUTFOLDER}"
-		executa "unzip -qq -o -d ${OUTFOLDER} ${DOWNLOAD}/ofLibs_${lib}_${PLATFORM}.zip"
-	done
+        executa mkdir -p "${OUTFOLDER}"
+        executa unzip -qq -o -d "${OUTFOLDER}" "${DOWNLOAD}/ofLibs_${lib}_${PLATFORM}.zip"
+    done
 }
 
+# ============================================
+# Main Execution
+# ============================================
+sectionOK "OpenFrameworks install ofLibs"
 
-sectionOK OpenFrameworks install ofLibs
-# ccache
 checkLib wget2
-#	fmt yaml-cpp
-# checkWget2
-executa mkdir -p ${DOWNLOAD}
+executa mkdir -p "${DOWNLOAD}"
 getlink
 unzipCore
 unzipAddons
-sectionOK Install ofLibs done
+
+sectionOK "Install ofLibs done"
 
 trap 'printf "${NC}"' EXIT
