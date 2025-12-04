a4e3b7a3c88e3aba403e855084b7201fd986c0fd
Dimitre <dimitre@users.noreply.github.com>
2025-12-04 12:48:24 -0300

    test libs.sh changes for windows 10

--- libs/libs.sh
+++ libs/libs.sh
@@ -182,12 +182,7 @@ case "$PLATFORM" in
     if ! command -v wget2 &>/dev/null; then
         section "wget2 not installed, installing scoop and wget2..."
         powershell.exe -NoProfile -ExecutionPolicy Bypass -Command "
-            # force load the module before Scoop installer tries to read policy
-            Import-Module Microsoft.PowerShell.Security -ErrorAction Stop
-            if (!(Get-Command scoop -ErrorAction SilentlyContinue)) {
-                & ([scriptblock]::Create((Invoke-RestMethod -Uri get.scoop.sh)))
-            }
-            scoop install wget2
+            & ([scriptblock]::Create((Invoke-RestMethod -Uri get.scoop.sh))) ; scoop install wget2
         "
 
         # --- 2.  make shim folder visible to *this* bash -----------
