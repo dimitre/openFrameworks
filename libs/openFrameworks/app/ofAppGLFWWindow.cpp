87e84a6109a908bcece5fee93290f15d85ae9bb2
Dimitre <dimitre@users.noreply.github.com>
2026-02-26 00:33:14 -0300

    wayland keyboard

--- libs/openFrameworks/app/ofAppGLFWWindow.cpp
+++ libs/openFrameworks/app/ofAppGLFWWindow.cpp
@@ -499,8 +499,30 @@ void ofAppGLFWWindow::update() {
 			waylandKeyStates.resize(WAYLAND_KEY_COUNT, GLFW_RELEASE);
 		}
 		
-		// Poll all possible keys
-		for (int keycode = GLFW_KEY_SPACE; keycode < WAYLAND_KEY_COUNT && keycode < (int)waylandKeyStates.size(); keycode++) {
+		// Poll common keys only (avoid invalid keycodes)
+		static const int keysToPoll[] = {
+			GLFW_KEY_SPACE, GLFW_KEY_APOSTROPHE, GLFW_KEY_COMMA, GLFW_KEY_MINUS, GLFW_KEY_PERIOD,
+			GLFW_KEY_SLASH, GLFW_KEY_0, GLFW_KEY_1, GLFW_KEY_2, GLFW_KEY_3, GLFW_KEY_4,
+			GLFW_KEY_5, GLFW_KEY_6, GLFW_KEY_7, GLFW_KEY_8, GLFW_KEY_9, GLFW_KEY_SEMICOLON,
+			GLFW_KEY_EQUAL, GLFW_KEY_A, GLFW_KEY_B, GLFW_KEY_C, GLFW_KEY_D, GLFW_KEY_E,
+			GLFW_KEY_F, GLFW_KEY_G, GLFW_KEY_H, GLFW_KEY_I, GLFW_KEY_J, GLFW_KEY_K,
+			GLFW_KEY_L, GLFW_KEY_M, GLFW_KEY_N, GLFW_KEY_O, GLFW_KEY_P, GLFW_KEY_Q,
+			GLFW_KEY_R, GLFW_KEY_S, GLFW_KEY_T, GLFW_KEY_U, GLFW_KEY_V, GLFW_KEY_W,
+			GLFW_KEY_X, GLFW_KEY_Y, GLFW_KEY_Z, GLFW_KEY_LEFT_BRACKET, GLFW_KEY_BACKSLASH,
+			GLFW_KEY_RIGHT_BRACKET, GLFW_KEY_GRAVE_ACCENT, GLFW_KEY_ESCAPE, GLFW_KEY_ENTER,
+			GLFW_KEY_TAB, GLFW_KEY_BACKSPACE, GLFW_KEY_INSERT, GLFW_KEY_DELETE, GLFW_KEY_RIGHT,
+			GLFW_KEY_LEFT, GLFW_KEY_DOWN, GLFW_KEY_UP, GLFW_KEY_PAGE_UP, GLFW_KEY_PAGE_DOWN,
+			GLFW_KEY_HOME, GLFW_KEY_END, GLFW_KEY_CAPS_LOCK, GLFW_KEY_SCROLL_LOCK,
+			GLFW_KEY_NUM_LOCK, GLFW_KEY_PRINT_SCREEN, GLFW_KEY_PAUSE, GLFW_KEY_F1,
+			GLFW_KEY_F2, GLFW_KEY_F3, GLFW_KEY_F4, GLFW_KEY_F5, GLFW_KEY_F6, GLFW_KEY_F7,
+			GLFW_KEY_F8, GLFW_KEY_F9, GLFW_KEY_F10, GLFW_KEY_F11, GLFW_KEY_F12,
+			GLFW_KEY_LEFT_SHIFT, GLFW_KEY_LEFT_CONTROL, GLFW_KEY_LEFT_ALT, GLFW_KEY_LEFT_SUPER,
+			GLFW_KEY_RIGHT_SHIFT, GLFW_KEY_RIGHT_CONTROL, GLFW_KEY_RIGHT_ALT, GLFW_KEY_RIGHT_SUPER
+		};
+		for (size_t i = 0; i < sizeof(keysToPoll)/sizeof(keysToPoll[0]); i++) {
+			int keycode = keysToPoll[i];
+			if (keycode >= (int)waylandKeyStates.size()) continue;
+			
 			int state = glfwGetKey(windowP, keycode);
 			int prevState = waylandKeyStates[keycode];
 			
@@ -1533,9 +1555,20 @@ void ofAppGLFWWindow::keyboard_cb(GLFWwindow * windowP_, int keycode, int scanco
 		key = codepoint = '=';
 		break;
 	default:
-		codepoint = keycodeToUnicode(instance, scancode, mods);
-//		codepoint = scancode;
-		key = codepoint;
+		// On Wayland, don't use X11-based keycodeToUnicode
+		#if defined(TARGET_LINUX)
+		if (glfwGetPlatform() == GLFW_PLATFORM_WAYLAND) {
+			// For printable ASCII keys, use the keycode directly
+			if (keycode >= 32 && keycode <= 126) {
+				codepoint = keycode;
+			}
+			key = codepoint;
+		} else
+		#endif
+		{
+			codepoint = keycodeToUnicode(instance, scancode, mods);
+			key = codepoint;
+		}
 		break;
 	}
 
