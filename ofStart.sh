653d32b8fd53a1fa696f1494c49fc393c6555411
Dimitre <dimitre@users.noreply.github.com>
2026-03-27 12:06:31 -0300

    try to fix chalet path for first install in windows

--- ofStart.sh
+++ ofStart.sh
@@ -4,7 +4,14 @@ set -e
 
 git pull
 
-./libs/libs.sh
+# Source libs.sh to inherit PATH changes (chalet installation)
+source ./libs/libs.sh
+
+# Also source the chalet PATH export if it exists
+if [[ -f ./libs/.tools/export_path.sh ]]; then
+    source ./libs/.tools/export_path.sh
+fi
+
 cd ofGen
 ./compile.sh
 #cd ../examples/demos/flechilhas
